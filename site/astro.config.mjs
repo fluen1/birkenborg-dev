539b0c09b31623e548037754fa616e92ba9e9e70
Philip Birkenborg <birkenborg.p@hotmail.com>
2026-05-06 01:24:56 +0200

    site: forbind til birkenborg.dev custom domain

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://birkenborg-dev.birkenborg-p.workers.dev',
+  site: 'https://birkenborg.dev',
   trailingSlash: 'never',
   build: { format: 'directory' },
   integrations: [sitemap()],
